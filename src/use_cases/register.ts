8050ee5939096d27fe0b99095ea8dd4ef85be3fd
williamjs95 <williamj.silva@live.com>
2024-06-10 19:38:25 -0300

    add use_cases

---
+++ src/use_cases/register.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseParams {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name, 
+  email, 
+  password }: RegisterUseCaseParams) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('User with same email already exists')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+}
