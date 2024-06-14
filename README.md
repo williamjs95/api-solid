2f7566fac179ade84c7c7093c1da1a46c80f57de
williamjs95 <williamj.silva@live.com>
2024-06-13 22:44:14 -0300

    add check-in use case

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ GymPass style app.
 
 - [x] It should be possible to register;
 - [x] It should be possible to authenticate;
-- [ ] It should be possible to retrieve the profile of a logged-in user;
+- [x] It should be possible to retrieve the profile of a logged-in user;
 - [ ] It should be possible to retrieve the number of check-ins performed by the logged-in user;
 - [ ] It should be possible for the user to retrieve their check-in history;
 - [ ] It should be possible for the user to search for nearby gyms;
--- src/use_cases/authenticate.spec.ts
+++ src/use_cases/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'johndoe@sample.com',
         password: 'password123',
@@ -44,7 +44,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('password123', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'johndoe@sample.com',
         password: 'password456',
