bd47231b80f1194200335c8b1fa6691ee15fb495
williamjs95 <williamj.silva@live.com>
2024-06-16 23:30:11 -0300

    chore: Update nearby gym search to include a 10km distance limit

---
+++ src/use_cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -22.9363278,
+      longitude: -47.087616,
+    })
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -10.7778057,
+      longitude: 75.5921968,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -22.9363278,
+      userLongitude: -47.087616,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
