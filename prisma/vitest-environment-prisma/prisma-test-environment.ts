6f9f0f124eaf0afa760d0ae6b94519ebafd9e0ad
williamjs95 <williamj.silva@live.com>
2024-06-18 18:09:20 -0300

    chore: Update prisma-test-environment.ts to generate a unique database URL for each test run

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,39 @@
+import 'dotenv/config'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 const prismaEnvironment: Environment = {
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const database_URL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = database_URL
+
+    execSync(`npx prisma migrate deploy`)
 
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
       transformMode: {
         ssr: [/\.ts$/],
