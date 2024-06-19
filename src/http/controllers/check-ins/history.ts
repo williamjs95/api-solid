287bc70e234e23a0917f2085da8156f6e3757bea
williamjs95 <williamj.silva@live.com>
2024-06-18 22:23:41 -0300

    chore: Add check-ins controllers

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use_cases/factories/make-fetch-users-check-ins-history-use-case'
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const checkInHistoryQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page } = checkInHistoryQuerySchema.parse(request.query)
+
+  const fethcUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+
+  const { checkIns } = await fethcUserCheckInsHistoryUseCase.execute({
+    userId: request.user.sub,
+    page,
+  })
+
+  return reply.status(201).send({
+    checkIns,
+  })
+}
